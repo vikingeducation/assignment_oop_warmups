455e07661586cfb88b6219565554501dea596a31
Thomas Lo <wagaiznogoud@gmail.com>
2016-01-05 09:53:39 -0800

    work on deep_dup

--- deep_dup.rb
+++ deep_dup.rb
@@ -1,3 +1,21 @@
-def deep_dup(array)
-  
+class Array
+
+  def deep_dup(array)
+
+    return 
+    result_arr = []
+
+    array.each do |ele|
+      deep_dup(ele) if ele.is_a?(Array)
+    end
+  end
 end
+
+
+deeper_arr = [ 
+                [ 1, [ 2, 3 ] ], 
+                [ 4, 5, 6], 
+                [ [ 7, 8, [ 9, 10 ]], [11, 12] ], 
+            ]
+
+puts deep_dup(deeper_arr)
