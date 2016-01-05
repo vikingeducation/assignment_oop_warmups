997d1c1f742520a33aba1f77fd67e7205bb0f959
Deepa Kackar <dkackar99@yahoo.com>
2016-01-05 10:19:20 -0800

    test

--- warmup2.rb
+++ warmup2.rb
@@ -1,5 +1,29 @@
 class Array
-  def deep_dup
-    
+
+  def deep_dup(new_arr=[])
+  	
+
+    self.each do |sub_a|
+
+    	if sub_a.class == Array && sub_a.all? {|item| item.class != Array}
+
+    		new_arr << sub_a
+
+    	else 
+            sub_a.deep_dup(new_arr)
+    	end	
+    end    
+    new_arr
   end
 end
+
+
+
+
+deeper_arr = [ 
+                [ 1, [ 2, 3 ] ], 
+                [ 4, 5, 6], 
+                [ [ 7, 8, [ 9, 10 ]], [11, 12] ], 
+            ]
+x = deeper_arr.deep_dup
+puts x.inspect            
