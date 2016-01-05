a5a0563505291d15e1803856389fb16c16701237
Thomas Lo <wagaiznogoud@gmail.com>
2016-01-05 10:44:32 -0800

    Finish deep_dup

--- deep_dup.rb
+++ deep_dup.rb
@@ -2,19 +2,24 @@ class Array
 
   def deep_dup
 
-    return self.dup
-    return self.deep_dup if self.is_a?(Array)
+    result = []
+    arr = self.dup
 
+    arr.each do |ele|
+      result << ele.deep_dup if ele.is_a?(Array)
+    end
+
+    return result
   end
 end
 
-arr = [ [1, 2], [3, 4] ]
-arr2 = arr.deep_dup
+# arr = [ [1, 2], [3, 4] ]
+# arr2 = arr.deep_dup
 
-puts arr.object_id
-puts arr2.object_id
-puts arr[0].object_id
-puts arr2[0].object_id
+# puts arr.object_id
+# puts arr2.object_id
+# puts arr[0].object_id
+# puts arr2[0].object_id
 
 deeper_arr = [ 
                 [ 1, [ 2, 3 ] ], 
@@ -22,3 +27,9 @@ deeper_arr = [
                 [ [ 7, 8, [ 9, 10 ]], [11, 12] ], 
             ]
 
+arr3 = deeper_arr.deep_dup
+
+puts arr3.object_id
+puts deeper_arr.object_id
+puts arr3[1].object_id
+puts deeper_arr[1].object_id
