5973067d86e92236130f31d8ef88541333251f09
Andrew Baik <and.baik@gmail.com>
2016-01-05 13:44:02 -0500

    finished deep_dup warmup2

--- warmup2.rb
+++ warmup2.rb
@@ -1,29 +1,30 @@
 class Array
 
-  def deep_dup(new_arr=[])
-  	
+  def deep_dup
 
+    new_array = []
+    # return self.dup if self.all? { |item| item.class != Array }
     self.each do |sub_a|
+      if sub_a.class != Array
+        new_array.push(sub_a)
+      else
+        new_array.push(sub_a.deep_dup)
+      end
+    end
 
-    	if sub_a.class == Array && sub_a.all? {|item| item.class != Array}
-
-    		new_arr << sub_a
-
-    	else 
-            sub_a.deep_dup(new_arr)
-    	end	
-    end    
-    new_arr
+    new_array
   end
+
 end
 
-
-
-
-deeper_arr = [ 
-                [ 1, [ 2, 3 ] ], 
-                [ 4, 5, 6], 
-                [ [ 7, 8, [ 9, 10 ]], [11, 12] ], 
+deeper_arr = [
+                [ 1, [ 2, 3 ] ],
+                [ 4, 5, 6],
+                [ [ 7, 8, [ 9, 10 ]], [11, 12] ],
             ]
+#puts deeper_arr.object_id
+puts deeper_arr[0][1].object_id
+
 x = deeper_arr.deep_dup
-puts x.inspect            
+#puts x.inspect.object_id
+puts x[0][1].object_id
