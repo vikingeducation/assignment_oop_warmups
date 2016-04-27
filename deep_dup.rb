a2a6c9c68633d1ec7e1de937900eab301ae2c7f2
Stephen Mayeux <stephenmayeux@gmail.com>
2016-04-28 03:31:42 +0900

    multidimensional array

--- deep_dup.rb
+++ deep_dup.rb
@@ -14,3 +14,59 @@ def deep_dup(array)
 
   return deep_copy
 end
+
+## for multidimensional arrays
+
+ddef deep_dup(array)
+
+  deep_copy = []
+
+  array.each do |value|
+	if value.any? { |inner| inner.is_a? Array }
+	  deep_copy << sub_arr(value)
+	else
+	  deep_copy << value.dup
+	end
+  end
+
+  return deep_copy
+end
+
+def sub_arr(value)
+  result = []
+  value.each do |number|
+  	if number.is_a? Fixnum
+  	  result << number
+  	elsif number.is_a? Array
+  	  result << number.dup
+  	end
+  end
+  return result
+end
+
+deeper_arr = [
+                [ 1, [ 2, 3 ] ],
+                [ 4, 5, 6],
+                [ [ 7, 8, [ 9, 10 ]], [11, 12] ]
+            ]
+
+puts deeper_arr.object_id
+puts deeper_arr[0].object_id
+puts deeper_arr[0][1].object_id
+puts deeper_arr[1].object_id
+puts deeper_arr[2].object_id
+puts deeper_arr[2][0].object_id
+puts deeper_arr[2][0][1].object_id
+puts deeper_arr[2][1].object_id
+puts
+
+test = deep_dup(deeper_arr)
+
+puts test.object_id
+puts test[0].object_id
+puts test[0][1].object_id
+puts test[1].object_id
+puts test[2].object_id
+puts test[2][0].object_id
+puts test[2][0][1].object_id
+puts test[2][1].object_id
