974acf1985d8be159ffeffe6a8282e664ee990c1
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-07-06 10:25:33 -0700

    Refact deep_dup

--- deep_dup.rb
+++ deep_dup.rb
@@ -4,19 +4,19 @@ class Array
     dup_array = []
     
     self.each do |item|
-      (item.is_a? Array) ? dup_array << item.deep_dup : 
-                         dup_array << dup_item(item)
+      
+      if item.is_a? Array
+        dup_array << item.deep_dup
+      else
+        begin 
+          dup_array << item.dup
+        rescue TypeError
+          dup_array << item
+        end
+      end
+
     end
     dup_array
   end
-
-  private
-    def dup_item(item)
-      if item.class.superclass.superclass == Numeric
-        item
-      else
-        item.dup
-      end
-    end
 
 end
