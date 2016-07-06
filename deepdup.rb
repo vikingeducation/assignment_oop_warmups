226b1eb586416b092fb163eef8e86bcd7315a790
chrisgoodson <f.chrisgoodson@gmail.com>
2016-07-06 11:51:03 -0500

    added methods to Array class

---
+++ deepdup.rb
@@ -0,0 +1,36 @@
+class Array
+
+
+
+	def 
+
+
+	def deep_dup
+
+		#change the number of loops depending on the nestedness of object
+
+		
+		# self.each do | layer1 |
+		# 	if layer1.is_a? Array 
+
+		# end
+
+
+
+		answer_array = []
+		i = 0
+
+		while i < self.length
+			j = 0
+			loop do 
+				if self[i].is_a? Array
+
+				end
+			end
+		end
+
+
+	end
+
+
+end
