419b825868ce584821c3f7f73532af69bb1eab50
Alex Lach <alexglach@gmail.com>
2016-07-06 12:52:19 -0400

    finishing deep dup

--- deep_dup.rb
+++ deep_dup.rb
@@ -5,11 +5,13 @@ class Array
     # if the item is not an array, return a duplicate of the item
     self.each do |item|
       if item.is_a? Array
-        dup_array << item.dup
+        dup_array << item.deep_dup
     # otherwise, for each item in array, deep duplicate its parts
       else
-        item.each do |inner_item|
-          dup_array << inner_item.deep_dup
+        if item.class.superclass.superclass == Numeric
+          dup_array << item
+        else
+          dup_array << item.dup
         end
       end
     end
