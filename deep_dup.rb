9a51226b2bd7d0974b23124dd4333e384eebc9ae
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-07-06 10:03:11 -0700

    Finish deep_dup

--- deep_dup.rb
+++ deep_dup.rb
@@ -2,20 +2,21 @@ class Array
 
   def deep_dup
     dup_array = []
-    # if the item is not an array, return a duplicate of the item
+    
     self.each do |item|
-      if item.is_a? Array
-        dup_array << item.deep_dup
-    # otherwise, for each item in array, deep duplicate its parts
-      else
-        if item.class.superclass.superclass == Numeric
-          dup_array << item
-        else
-          dup_array << item.dup
-        end
-      end
+      (item.is_a? Array) ? dup_array << item.deep_dup : 
+                         dup_array << dup_item(item)
     end
     dup_array
   end
+
+  private
+    def dup_item(item)
+      if item.class.superclass.superclass == Numeric
+        item
+      else
+        item.dup
+      end
+    end
 
 end
