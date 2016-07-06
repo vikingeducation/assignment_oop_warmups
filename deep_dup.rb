6c9043e248583c51ef2795efb40fd6812b7d9b87
Adrian Mui <adrian.mui@gmail.com>
2016-07-06 13:29:02 -0400

    deep_dup recursively

--- deep_dup.rb
+++ deep_dup.rb
@@ -4,5 +4,19 @@ class Array
     data = Marshal.dump(self)
     Marshal.load(data)
   end
+
+  def deep_dup_two
+    temp_arr = []
+    self.each do |item|
+      temp_arr << (item.is_a?(Integer) ? item : item.deep_dup_two)
+    end
+    temp_arr
+  end
 end
+
+arr = [ [1, [1,2]], [3, 4] ]
+
+duper = arr.deep_dup_two
+
+puts arr[0][1].equal?(duper[0][1])
 
