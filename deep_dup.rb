6444dce9d619c3ad3878cd73a12c67e0386d0a55
Visiona <darbis@gmail.com>
2016-10-10 20:34:43 +0100

    Created solution to deep_dup

--- deep_dup.rb
+++ deep_dup.rb
@@ -1,29 +1,17 @@
 class Array
-  def deep_dup(rec_arr = nil)
+  def deep_dup
     self.map do |arr1|
-      if arr1 != arr1.flatten
-        arr1.map {|x| x.dup}
-      else
-        arr1 = arr1.dup
-      end
+      arr1.is_a?(Array) ? arr1.deep_dup : arr1
     end
   end
 end
 
 
+
 # class Array
-#   def deep_dup(rec_arr = nil)
-#     self.map do |arr1| 
-#       if arr1 != arr1.flatten
-#         deep_dup(arr1)
-#       else
-#         arr1.dup
-#       end
-#     end
+#   def deep_dup
+#     Marshal.load( Marshal.dump(self))
 #   end
 # end
-
-
-
 
 # deeper_arr = [ [ 1, [ 2, 3 ] ], [ 4, 5, 6], [ [ 7, 8, [ 9, 10 ]], [11, 12] ] ]
