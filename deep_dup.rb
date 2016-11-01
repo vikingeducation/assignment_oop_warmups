5a9025c603ccb1f95978e7b5835d33f35dbc7c53
luke-schleicher <luke.j.schleicher@gmail.com>
2016-11-01 11:53:54 -0500

    11:53

--- deep_dup.rb
+++ deep_dup.rb
@@ -1,11 +1,25 @@
 class Array
+
   def deep_dup
-    #Createa a new array
+    # Create a new array
+    output = []
     # loop over each item in outter most array
-      #make copy of inner most array
-      #push it to a new array
-    #return that new array
-
-
+    each do |item|
+      temp
+      # if the class of this item in an array
+        # then recurse
+      # otherwise dup that item
+      # push it to a new array
+    # return that new array
+    end
   end
+
 end
+
+arr = [[1,2],[3,4]]
+puts arr.object_id
+
+puts ""
+
+duper = arr.deep_dup
+puts arr.object_id
