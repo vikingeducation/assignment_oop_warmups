d75cd920682f0c62f8391a43c5f092218c95a6dc
luke-schleicher <luke.j.schleicher@gmail.com>
2016-11-01 11:03:03 -0500

    11:03

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # assignment_oop_warmups
 Mmmm... modularity..
 
-Completed by Luke Schleicher
+Completed by Luke Schleicher and Kenny Kottenstette
---
+++ ttt_interfaces.rb
@@ -0,0 +1,16 @@
+Class: TicTacToe
+  play
+  check_game_over
+  check_victory
+  check_draw
+  switch_players
+
+Class: Player
+  get_coordinates
+  ask_for_coordinates
+  validate_coordinates_format(coords)
+
+Class: Board
+  render
+  add_piece(coords, piece)
+  
