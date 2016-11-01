97ae7453a40938f6657cd165fd79d0d40f28157f
Jessica G <gillan.jessica@gmail.com>
2016-11-01 10:30:04 -0600

    deep_dup

--- deep_dup.rb
+++ deep_dup.rb
@@ -1,11 +1,15 @@
-def deep_dup(multi_d_arr)
-	new_arr = multi_d_arr.dup
 
-	arr_to_check = new_arr # new_arr.dup
-	while arr_to_check[0].class == Array
-		arr_to_check.map! do |element|
-			element.dup
+class Array
+
+	def deep_dup(multi_d_arr)
+		new_arr = multi_d_arr.dup
+
+		arr_to_check = new_arr # new_arr.dup
+		while arr_to_check[0].class == Array
+			arr_to_check.map! do |element|
+				element.dup
+			end
+			arr_to_check = arr_to_check[0]
 		end
-		arr_to_check = arr_to_check[0]
 	end
 end
