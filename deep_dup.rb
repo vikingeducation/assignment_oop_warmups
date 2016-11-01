be9701d4251443bdba1fa475da5df17efdc906b9
Catherine Kwak <khopsickle@users.noreply.github.com>
2016-11-01 09:19:17 -0700

    Finish deep_dup

--- deep_dup.rb
+++ deep_dup.rb
@@ -1,18 +1,53 @@
 class Array
 
   def deep_dup
-    duper = []
-    # iterate through outter array
-    self.each do |item|
+    # duper = []
+    # # iterate through outter array
+    # self.each do |item|
+    #   if item.is_a?(Array)
+    #     item.deep_dup
+    #   else
+    #     duper << item
+    #   end
+    # end
+    # duper
+
+    self.map do |item|
       if item.is_a?(Array)
         item.deep_dup
       else
-        duper << item
+        item
       end
     end
-    duper
   end
 
-
-
 end
+
+
+arr = [ [1, 2], [3, 4] ]
+
+# Verify their object_ids
+p arr.object_id
+#=> 70349854069800
+p arr[0].object_id
+#=> 70349854069960
+p arr[1].object_id
+#=> 70349854069840
+
+p duper = arr.deep_dup
+
+# Verify they are different ids
+p duper.object_id
+#=> 70349854047610
+p duper[0].object_id
+#=> 70349854041100
+p duper[1].object_id
+#=> 70342030583020
+
+deeper_arr = [
+                [ 1, [ 2, 3 ] ],
+                [ 4, 5, 6],
+                [ [ 7, 8, [ 9, 10 ]], [11, 12] ],
+            ]
+
+p deeper_arr.deep_dup
