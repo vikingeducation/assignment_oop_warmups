3c2181cabf17f2fd804a15925808f4d7e7b6c551
Pramod Jacob <psjacob68@gmail.com>
2016-11-01 13:39:47 -0400

    refactored deep_dup.rb and completed tweets.rb

--- deep_dup.rb
+++ deep_dup.rb
@@ -1,17 +1,13 @@
 class Array
 
 	def deep_dup
-		new_arr = self.dup
-		# print self.inspect + " " 
-		# puts self.object_id
-		# print new_arr.inspect + " "
-		# puts new_arr.object_id 
-		new_arr.each_with_index do |item, index| # self = [1,2,[3,4],5]
-			if item.class == Array
-        new_arr[index] = item.deep_dup
+		map do |item| 
+			if item.is_a?(Array)
+        item.deep_dup
+      else
+      	item
       end
 		end
-    new_arr
 	end
 
 end
@@ -19,11 +15,5 @@ end
 arr = [ [ 1, [ 2, 3 ] ],[ 4, 5, 6],[ [ 7, 8, [ 9, 10 ]], [11, 12] ]]
 test_arr = arr.deep_dup
 
-# puts arr.object_id
-# puts test_arr.object_id
-# puts "----------------"
-# puts arr[2].object_id
-# puts test_arr[2].object_id
-# puts "-----------"
-# puts arr[2][0].object_id
-# puts test_arr[2][0].object_id
+puts arr[0][1][0].object_id
+puts test_arr[0][1][0].object_id
