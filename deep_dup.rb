774f47467143d9718f698a15f9a433fe2bf3423a
Jessica G <gillan.jessica@gmail.com>
2016-11-01 10:35:35 -0600

    deep_dup get up to date

--- deep_dup.rb
+++ deep_dup.rb
@@ -1,21 +1,8 @@
-
 class Array
-
 	def deep_dup
-		# duplicate the outer array 
-		# check if array is multi dimensional
-		# if yes - duplicate inner array
-
-		# 
-
-		# new_arr = self.dup
-
-		# arr_to_check = new_arr # new_arr.dup
-		# while arr_to_check[0].class == Array
-		# 	arr_to_check.map! do |element|
-		# 		element.dup
-		# 	end
-		# 	arr_to_check = arr_to_check[0]
-		# end
+		self.map do |element|
+			next unless element.class == Array.
+			element.deep_dup
+		end
 	end
 end
