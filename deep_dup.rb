03fabce939cc4871fdca7641a8d5ee59a25d3044
Jake Tingley <jaket32@gmail.com>
2016-11-01 12:43:37 -0400

    working on deep_dup.rb

--- deep_dup.rb
+++ deep_dup.rb
@@ -4,9 +4,25 @@ class Array
 		new_arr = []
 		self.each do |item| # self = [1,2,[3,4],5]
 			if item.class == Array
-			new_arr << item
-			new_arr << item.dup if item.class == "Array"
+        puts "array"
+        new_arr << item.dup
+      else
+        puts "not an array"
+			  new_arr << item
+      end
 		end
 	end
 
 end
+
+arr = [1,2,[[3],4],5]
+test_arr = arr.deep_dup
+
+puts arr.object_id
+puts test_arr.object_id
+
+puts arr[2].object_id
+puts test_arr[2].object_id
+
+puts arr[2][0].object_id
+puts test_arr[2][0].object_id
