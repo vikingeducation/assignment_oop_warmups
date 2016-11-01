dc3de92b2969574548657f9adef253b8d266262c
Kenneth Kottenstette <kennykotten@gmail.com>
2016-11-01 11:29:50 -0700

    finish oo warmups 2

--- deep_dup.rb
+++ deep_dup.rb
@@ -1,47 +1,11 @@
-require 'pry'
-
 class Array
-
   def deep_dup
-
-    # loop over each item in outter most array
     map do |item|
-      # if item is an array
       if item.is_a?(Array)
         item.deep_dup
       else
         item
       end
     end
-
   end
-
 end
-
-
-# deeper_arr = [
-#                 [ 1, [ 2, 3 ] ],
-#                 [ 4, 5, 6],
-#                 [ [ 7, 8, [ 9, 10 ]], [11, 12] ],
-#             ]
-
-# p deeper_arr
-# p deeper_arr.object_id
-# p deeper_arr[0].object_id
-# p deeper_arr[0][1].object_id
-# p deeper_arr[1].object_id
-
-# puts ""
-# duper = deeper_arr.deep_dup
-
-# p duper
-# p duper.object_id
-# p duper[0].object_id
-# p duper[0][1].object_id
-# p duper[1].object_id
-
-# duper = []
-# p deeper_arr
-
-
-
--- including_enumerable.rb
+++ including_enumerable.rb
@@ -1,37 +1,16 @@
 class Tweeter
-  #include Enumerable
+  include Enumerable
     def initialize
         @tweets = []
     end
 
     def tweet(message)
-        # Your TODO: fill this in. 
-        # This should add the first 144 characters
-        # of any message to the @tweets array
-
-        @tweets << message
+        @tweets << message[0,143]
     end
 
     def each
-      @tweets.size.times do |i|
-        yield(self[i])
+      @tweets.each do |i|
+        yield(i)
       end
     end
 end
-
-
-t = Tweeter.new
-
-# Add some tweets to our @tweets array
-t.tweet("first message")
-t.tweet("second message")
-
-# Use Enumerable methods on the Tweeter instance
-t.each{|msg| puts msg}
-#=> first message
-#=> second message
-
-# Note: You should NOT have defined this method explicitly!
-#   ...it should come from Enumerable by default.
-t.map{|msg| msg.upcase}
-#=> ["FIRST MESSAGE", "SECOND MESSAGE"]
