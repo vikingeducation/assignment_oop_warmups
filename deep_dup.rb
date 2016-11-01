80674a5fc887457835b2bce67a308a672db60da4
Pramod Jacob <psjacob68@gmail.com>
2016-11-01 12:58:42 -0400

    completed deep_dup.rb

--- deep_dup.rb
+++ deep_dup.rb
@@ -2,6 +2,10 @@ class Array
 
 	def deep_dup
 		new_arr = self.dup
+		# print self.inspect + " " 
+		# puts self.object_id
+		# print new_arr.inspect + " "
+		# puts new_arr.object_id 
 		new_arr.each_with_index do |item, index| # self = [1,2,[3,4],5]
 			if item.class == Array
         new_arr[index] = item.deep_dup
