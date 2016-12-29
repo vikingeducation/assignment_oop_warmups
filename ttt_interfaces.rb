b9c3a7a7103a464958002cfdfa98fc70646c0771
andie5 <andieasa@googlemail.com>
2016-12-29 12:33:08 +0000

    removed def keywords and the initialize functions and added attr accessor lines

--- ttt_interfaces.rb
+++ ttt_interfaces.rb
@@ -1,29 +1,11 @@
 # Notes for a TicTacToe interface
 Class: TicTacToe
 
-    # initialize
-    def initialize
-        # set up the board
-        @board = Board.new
-
-        # set up the players
-        @player_x = Player.new("Madame X", :x, @board)
-        @player_y = Player.new("Mister Y", :y, @board)
-
-        # assign the starting player
-        @current_player = @player_x
-    end
-
-    # play
     play
 
-private
     check_game_over
-
     check_victory
-
     check_draw
-
     switch_players
 
 end
@@ -34,29 +16,14 @@ end
 # Manages all player-related functionality
 Class: Player
     name
+    name=(value)
     piece
+    piece=(value)
 
-    # initialize
-    def initialize(name = "Mystery_Player", piece, board)
-        # Set marker type (e.g. X or O)
-        raise "Piece must be a Symbol!" unless piece.is_a?(Symbol)
-        @name = name
-        @piece = piece
-        @board = board
-    end
+    get_coordinates
+    ask_for_coordinates
 
-    # get_coordinates
-    def get_coordinates
-
-    # ask_for_coordinates
-    def ask_for_coordinates
-
-
-  private
-    # validate_coordinates_format
-    def validate_coordinates_format(coords)
-    
-
+    validate_coordinates_format(coords)
 end
 
 
@@ -65,57 +32,23 @@ end
 # Maintains game board state
 Class: Board
 
-    # Allow reading the @board_arr variable
     board_arr
 
-    # initialize board
-    def initialize( board_arr = nil )
-        # set up blank data structure unless
-        # we are passed one
-        @board_arr = board_arr || Array.new(3){Array.new(3)}
-    end
-
-    # render
     render
-
-    # add_piece
     add_piece(coords, piece)
     
-    private
-
-    # piece_location_valid?
     piece_location_valid?(coords)
-
-    # within_valid_coordinates?
     within_valid_coordinates?(coords)
-    
-    # coordinates_available?
     coordinates_available?(coords)
-     
-    # winning_combination?
     winning_combination?(piece)
-
-    # winning_diagonal?
     winning_diagonal?(piece)
-     
-    # winning_vertical?
     winning_vertical?(piece)
-    
-    # winning_horizontal?
     winning_horizontal?(piece)
-    
 
-    # diagonals
     diagonals
-    
-    # verticals
     verticals
     
-    # horizontals
     horizontals
-    
-    # full?
     full?
-    
 
 end
