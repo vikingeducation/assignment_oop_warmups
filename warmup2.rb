7f4b86dc9d3d97a26661fb62eda50ce6c4ea4508
Roy Chen <roychenlei@gmail.com>
2017-02-06 14:17:11 +0800

    pseudocoded deep_dup method

---
+++ warmup2.rb
@@ -0,0 +1,11 @@
+class Array
+  
+  # Pseudocode:
+  # 1. Create an output array to store duplicated items
+  # 2. For every item in the input array, check whether it's an array
+  # 3. If the item is not an array, dup it and add it to the output array
+  # 4. If the item is an array, recursively call deep_dup on it, and push 
+  # results to the output array
+  def deep_dup
+  end
+end
