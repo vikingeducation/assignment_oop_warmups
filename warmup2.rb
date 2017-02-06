2084dc9b64f8756b1503897856b3dd6fc62ecead
Roy Chen <roychenlei@gmail.com>
2017-02-06 20:58:11 +0800

    completed Warmup 2

--- warmup2.rb
+++ warmup2.rb
@@ -7,5 +7,27 @@ class Array
   # 4. If the item is an array, recursively call deep_dup on it, and push 
   # results to the output array
   def deep_dup
+    deep_dups = []
+    self.each do |item|
+      duped_item = nil
+      if item.is_a?(Array)
+        duped_item = item.deep_dup
+      else
+        duped_item = item
+      end
+      
+      deep_dups << duped_item
+    end
+
+    deep_dups
+  end
+
+  # method to recursively check the object ids of nested arrays
+  def display_object_ids
+    self.each.with_index do |item, index|
+      puts "Item #{item} at index #{index} has ID #{item.object_id}"
+
+      item.display_object_ids if item.is_a?(Array)
+    end
   end
 end
