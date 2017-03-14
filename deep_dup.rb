35d9558dba95e578f0475662cd5c62bb06cee79d
Brent Guffens <brentguf@users.noreply.github.com>
2017-03-14 15:20:17 +0100

    deep_dup

---
+++ deep_dup.rb
@@ -0,0 +1,21 @@
+class Array
+	def deep_dup
+	  self.map { |item| item.is_a?(Array) ? item.deep_dup : item }
+	end
+end
+
+arr = [[1, 2], [3, 4]]
+
+deeper_arr = [ 
+  [1, [2, 3]], 
+  [4, 5, 6], 
+  [[7, 8, [9, 10]], [11, 12]], 
+]
+
+print arr.object_id
+puts
+print arr.deep_dup.object_id
+puts
+print deeper_arr.object_id
+puts
+print deeper_arr.deep_dup.object_id
