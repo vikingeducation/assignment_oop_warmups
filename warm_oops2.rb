764f6d2f6541646f97046e828c1e2527862806fe
HoratioFox <etgt28@mail.missouri.edu>
2017-04-05 01:31:56 -0500

    Interface warmup finished

---
+++ warm_oops2.rb
@@ -0,0 +1,20 @@
+#oops warmup 2
+
+#Interfaces : make the interface for all the classes in the Tic Tac Toe Demo
+
+Class:  TicTacToe
+  def play ()
+
+Class: Player
+  name
+  piece
+  piece=(new_piece)
+  name=(new_name)
+  def get_coordinates
+
+Class: Board
+  board_arr
+  def render
+  def winning_combination?(piece)
+  def full?
+  def add_piece(coords, piece)
