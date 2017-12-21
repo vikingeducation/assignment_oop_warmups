b768dbd25cf4d1c5511c3292fbc6a8ff0a8a3091
Brennan <31666354+BrennanFulmer@users.noreply.github.com>
2017-12-21 15:32:28 -0500

    saving before rework, realized this doesn't work more than 1 layer of array's deep

--- deep_dup.rb
+++ deep_dup.rb
@@ -5,33 +5,44 @@
  simple dup method will be useful but not the only thing you use.
 =end
 
+require "pry"
+
 base = [ ["red", "blue", "green"], [1, 2, 3] ]
-puts "base's object id is #{base.object_id}"
-puts "the object id of the first value of base is #{base[0].object_id}"
+puts "\nbase's id is #{base.object_id}"
+puts "\nid of 1st value in base is #{base[0].object_id}"
+puts "\nid of 1st value in 1st array in base is #{base[0][0].object_id}"
 
 super_base = [
-               [ 1, [ 2, 3 ] ],
-               [ 4, 5, 6],
-               [ [ 7, 8, [ 9, 10 ]], [11, 12] ],
-            ]
-puts "super_base's object id is #{super_base.object_id}"
-puts "the object id of the first value of super_base is #{super_base[0].object_id}"
+                [ 1, [ 2, 3 ] ],
+                [ 4, 5, 6],
+                [ [ 7, 8, [ 9, 10 ]], [11, 12] ],
+             ]
+puts "\nsuper_base's id is #{super_base.object_id}"
+puts "\nid of 1st value in super_base is #{super_base[0].object_id}"
+puts "\nid of 1st value in 1st array in super_base is #{super_base[0][0].object_id}"
 
 public
 def deep_dup(array)
   dupped = []
-    array.each do |tweak|
-      dupped << tweak.dup
+    array.each do |value|
+      dupped << value.dup
     end
-    flipped = dupped.reverse
-    new_thing = flipped.reverse
-    new_thing
+      dupped.each do |item|
+        deep_dup(item) if item.is_a?(Array)
+      end
+      flipped = dupped.reverse
+      new_thing = flipped.reverse
+      new_thing
 end
 
 dupe = deep_dup(base)
-puts "the dupe of base's object id is #{dupe.object_id}"
-puts "the object id of the first value of base's dupe is #{dupe[0].object_id}"
+puts "\ndupe of base's id is #{dupe.object_id}"
+puts "\nid of 1st value in base's dupe is #{dupe[0].object_id}"
+puts "\nid of 1st value in 1st array in base's dupe is #{dupe[0][0].object_id}"
 
 duper = deep_dup(super_base)
-puts "the dupe of super_base's object id is #{duper.object_id}"
-puts "the object id of the first value of super_base's dupe is #{duper[0].object_id}"
+puts "\ndupe of super_base's id is #{duper.object_id}"
+puts "\nid of the 1st value in super_base's dupe is #{duper[0].object_id}"
+puts "\nid of 1st value in 1st array in super_base's dupe is #{duper[0][0].object_id}\n"
+
+binding.pry
