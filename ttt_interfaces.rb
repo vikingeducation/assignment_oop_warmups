df69145ac070bf23260a7fa61b31a70d37c43c0c
Brennan <31666354+BrennanFulmer@users.noreply.github.com>
2017-12-21 03:51:39 -0500

    tic tac toe interface list done

---
+++ ttt_interfaces.rb
@@ -0,0 +1,21 @@
+
+=begin
+  Go back to the Tic Tac Toe demo and write down the interface for each of the
+  classes it uses. This means indicating which methods and variables are
+  publicly available and how to call them.
+=end
+
+class TicTacToe
+  play()
+
+class Player
+  name
+  name = (new_value)
+  piece
+  piece = (new_value)
+  get_coordinates()
+
+class Board
+  board_arr
+  render()
+  
