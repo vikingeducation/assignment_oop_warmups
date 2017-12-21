60181c38d8b2a7288895957ca3a25ec9c2f25936
Brennan <31666354+BrennanFulmer@users.noreply.github.com>
2017-12-21 03:54:47 -0500

    starting on deep dup

---
+++ deep dup.rb
@@ -0,0 +1,6 @@
+
+=begin
+ Pass-by-reference is an important concept. Create a method deep_dup which
+ returns a completely new object on a 2-or-more dimensional array. Ruby's
+ simple dup method will be useful but not the only thing you use.
+=end
