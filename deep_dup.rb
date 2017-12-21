66f2e22c1aa6cbd4af6bc844c1e335a740ebd702
Brennan <31666354+BrennanFulmer@users.noreply.github.com>
2017-12-21 04:52:26 -0500

    finished deep dup

--- deep_dup.rb
+++ deep_dup.rb
@@ -5,8 +5,33 @@
  simple dup method will be useful but not the only thing you use.
 =end
 
-require "pry"
-
 base = [ ["red", "blue", "green"], [1, 2, 3] ]
+puts "base's object id is #{base.object_id}"
+puts "the object id of the first value of base is #{base[0].object_id}"
 
-binding.pry
+super_base = [
+               [ 1, [ 2, 3 ] ],
+               [ 4, 5, 6],
+               [ [ 7, 8, [ 9, 10 ]], [11, 12] ],
+            ]
+puts "super_base's object id is #{super_base.object_id}"
+puts "the object id of the first value of super_base is #{super_base[0].object_id}"
+
+public
+def deep_dup(array)
+  dupped = []
+    array.each do |tweak|
+      dupped << tweak.dup
+    end
+    flipped = dupped.reverse
+    new_thing = flipped.reverse
+    new_thing
+end
+
+dupe = deep_dup(base)
+puts "the dupe of base's object id is #{dupe.object_id}"
+puts "the object id of the first value of base's dupe is #{dupe[0].object_id}"
+
+duper = deep_dup(super_base)
+puts "the dupe of super_base's object id is #{duper.object_id}"
+puts "the object id of the first value of super_base's dupe is #{duper[0].object_id}"
